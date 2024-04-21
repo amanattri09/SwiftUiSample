a0e7e4622d94b95581e50990578b97525a36d824
Aman Attri <aman.attri@assurant.com>
2024-04-21 19:21:14 +0530

    Initial Commit

---
+++ SwiftUiLearning/SwiftUiLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiLearningApp.swift
+//  SwiftUiLearning
+//
+//  Created by Aman Attri on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
